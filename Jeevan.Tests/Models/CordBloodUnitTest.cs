f3bdec65c89046c4e212edd2562fd83edc9a3a24
Prasanth Guruprasad <gprasant@thoughtworks.com>
2012-07-04 06:41:11 +0530

    Added Display format for HLA types and added formatting on the td elements to align

---
+++ Jeevan.Tests/Models/CordBloodUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Jeevan.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jeevan.Tests.Models
+{
+    [TestClass]
+    public class CordBloodUnitTest
+    {
+        private CordBloodUnit unit = new CordBloodUnit();
+        
+        [TestMethod]
+        public void Should_Have_D2_Display_Format_Property()
+        {
+            AssertDisplayableAttribute("HLA_A1", typeof(CordBloodUnit), typeof(DisplayFormatAttribute), "{0:D2}");
+            AssertDisplayableAttribute("HLA_A2", typeof(CordBloodUnit), typeof(DisplayFormatAttribute), "{0:D2}");
+            AssertDisplayableAttribute("HLA_B1", typeof(CordBloodUnit), typeof(DisplayFormatAttribute), "{0:D2}");
+            AssertDisplayableAttribute("HLA_B2", typeof(CordBloodUnit), typeof(DisplayFormatAttribute), "{0:D2}");
+            AssertDisplayableAttribute("HLA_C1", typeof(CordBloodUnit), typeof(DisplayFormatAttribute), "{0:D2}");
+            AssertDisplayableAttribute("HLA_C2", typeof(CordBloodUnit), typeof(DisplayFormatAttribute), "{0:D2}");
+
+            AssertDisplayableAttribute("DRB_1", typeof(CordBloodUnit), typeof(DisplayFormatAttribute), "{0:D2}");
+            AssertDisplayableAttribute("DRB_2", typeof(CordBloodUnit), typeof(DisplayFormatAttribute), "{0:D2}");
+            AssertDisplayableAttribute("DQB_1", typeof(CordBloodUnit), typeof(DisplayFormatAttribute), "{0:D2}");
+            AssertDisplayableAttribute("DQB_2", typeof(CordBloodUnit), typeof(DisplayFormatAttribute), "{0:D2}");
+        }
+
+        private void AssertDisplayableAttribute(string propertyName, Type domainType, Type attrType, string expectedDataFormat)
+        {
+            var prop = domainType.GetProperty(propertyName);
+            Assert.IsTrue(Attribute.IsDefined(prop, attrType), string.Format("{0} did not have DisplayFormatAttribute", propertyName));
+            var formatString = (prop.GetCustomAttributes(attrType, false)[0] as DisplayFormatAttribute).DataFormatString;
+            Assert.AreEqual(expectedDataFormat, formatString);
+        }
+    }
+}
--- Jeevan/Models/CordBloodUnit.cs
+++ Jeevan/Models/CordBloodUnit.cs
@@ -6,17 +6,27 @@ namespace Jeevan.Models
     {
         public int ID { get; set; }
         
+        [DisplayFormat(DataFormatString = "{0:D2}")]
         public int? HLA_A1 { get; set; }
+        [DisplayFormat(DataFormatString = "{0:D2}")]
         public int? HLA_A2 { get; set; }
+        [DisplayFormat(DataFormatString = "{0:D2}")]
         public int? HLA_B1 { get; set; }
+        [DisplayFormat(DataFormatString = "{0:D2}")]
         public int? HLA_B2 { get; set; }
-        
+
+        [DisplayFormat(DataFormatString = "{0:D2}")]
         public int DRB_1 { get; set; }
+        [DisplayFormat(DataFormatString = "{0:D2}")]
         public int DRB_2 { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:D2}")]
         public int HLA_C1 { get; set; }
+        [DisplayFormat(DataFormatString = "{0:D2}")]
         public int HLA_C2 { get; set; }
+        [DisplayFormat(DataFormatString = "{0:D2}")]
         public int DQB_1 { get; set; }
+        [DisplayFormat(DataFormatString = "{0:D2}")]
         public int DQB_2 { get; set; }
     }
 }
