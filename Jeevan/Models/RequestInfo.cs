c59381cfb949d5821dbc296889f11dd168bef9a8
Prasanth Guruprasad <gprasant@thoughtworks.com>
2012-08-18 20:06:39 +0530

    fixing build

---
+++ Jeevan/Models/RequestInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jeevan.Models
+{
+    public class RequestInfo
+    {
+        [Required]
+        [DisplayName("Patient's Name")]
+        public string PatientName { get; set; }
+
+        [DisplayName("Patient's Date of birth")]
+        public DateTime PatientDateOfBirth { get; set; }
+
+        [Required]
+        [DisplayName("Patient's Gender")]
+        public string Gender { get; set; }
+
+        public string Hospital { get; set; }
+        [DisplayName("Physician's Name"),Required]
+        public string PhysicianName { get; set; }
+
+        [DisplayName("Physician's email address"),Required]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Please Enter a valid Email")]
+        public string PhysicianEmail { get; set; }
+
+        [DisplayName("Physician's phone number")]
+        [RegularExpression(@"^[+]{0,1}\d+$", ErrorMessage = "Enter a valid phone number")]
+        public string PhysicanPhoneNumber { get; set; }
+
+        public string Diagnosis { get; set; }
+    }
+}
